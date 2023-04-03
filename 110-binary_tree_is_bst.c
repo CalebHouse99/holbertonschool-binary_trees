db17c4ee309ae72fcbabb437d644e57195070daa
CalebHouse99 <78427735+CalebHouse99@users.noreply.github.com>
2023-04-03 11:26:33 -0500

    added to header file

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,24 +1,6 @@
 #include <limits.h>
 #include "binary_trees.h"
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-struct binary_tree_s
-{
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
-
 int is_bst_helper(const binary_tree_t *tree, int min, int max)
 {
     if (tree == NULL)
