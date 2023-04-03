3859fcaac424d409b80f9d3a4ced807343fe0219
CalebHouse99 <78427735+CalebHouse99@users.noreply.github.com>
2023-04-03 11:17:39 -0500

    added struct

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,25 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <limits.h>
 #include "binary_trees.h"
 
-typedef struct binary_tree_s
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
 {
-    int value;
+    int n;
+    struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-} binary_tree_t;
+};
+
+typedef struct binary_tree_s binary_tree_t;
 
 int is_bst_helper(const binary_tree_t *tree, int min, int max)
 {
     if (tree == NULL)
         return 1;
 
-    if (tree->value < min || tree->value > max)
+    if (tree->n < min || tree->n > max)
         return 0;
 
-    return is_bst_helper(tree->left, min, tree->value - 1) &&
-           is_bst_helper(tree->right, tree->value + 1, max);
+    return is_bst_helper(tree->left, min, tree->n - 1) &&
+           is_bst_helper(tree->right, tree->n + 1, max);
 }
 
 int binary_tree_is_bst(const binary_tree_t *tree)
